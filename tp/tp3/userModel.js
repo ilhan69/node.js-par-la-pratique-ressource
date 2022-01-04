baf97d1e78dac9beceafe87a9b6d24861decb8da
ilhan69 <68482895+ilhan69@users.noreply.github.com>
2022-01-04 18:11:35 +0100

    tp 3

---
+++ tp/tp3/userModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('./db');
+const userModel = mongoose.model('User', 
+{ 
+    id: Number,
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    phone: String,
+    creationDate: Date,
+    role: String,
+ });
+
+module.exports = userModel;
